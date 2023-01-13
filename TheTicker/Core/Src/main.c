18d24ac4200e86df9780bccc1bf29c7434101d3a
Ulad Shumeika <wladshumeiko228@gmail.com>
2023-01-13 16:44:13 +0100

    [project] feat: add the project's base

---
+++ TheTicker/Core/Src/main.c
@@ -0,0 +1,65 @@
+/*
+ * The pet project -
+ *
+ *  Created on: Jan 13, 2023
+ *      Author: Ulad Shumeika
+ */
+
+//---------------------------------------------------------------------------
+// Includes
+//---------------------------------------------------------------------------
+#include "main.h"
+
+//---------------------------------------------------------------------------
+// Static function prototypes
+//---------------------------------------------------------------------------
+static void SystemClock_Config(void);
+
+//---------------------------------------------------------------------------
+// Main function
+//---------------------------------------------------------------------------
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+	SystemClock_Config();
+
+	// Call init function for freertos objects (in freertos.c)
+	MX_FREERTOS_Init();
+
+	// Start scheduler
+	osKernelStart();
+
+    /* Loop forever */
+	for(;;);
+}
+
+//---------------------------------------------------------------------------
+// Initialization functions
+//---------------------------------------------------------------------------
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+	printf("Wrong parameters value: file %s on line %ld\r\n", file, line);
+}
+#endif /* USE_FULL_ASSERT */
---
+++ TheTicker/Core/Src/stm32f4xx_it.c
@@ -0,0 +1,115 @@
+//---------------------------------------------------------------------------
+// Includes
+//---------------------------------------------------------------------------
+#include "main.h"
+#include "stm32f4xx_it.h"
+
+//---------------------------------------------------------------------------
+// External variables
+//---------------------------------------------------------------------------
+
+/******************************************************************************/
+/*           Cortex-M4 Processor Interruption and Exception Handlers          */
+/******************************************************************************/
+
+/**
+  * @brief This function handles Non maskable interrupt.
+  */
+void NMI_Handler(void)
+{
+	while(1)
+	{
+
+	}
+}
+
+/**
+  * @brief This function handles Hard fault interrupt.
+  */
+void HardFault_Handler(void)
+{
+	while(1)
+	{
+
+	}
+}
+
+/**
+  * @brief This function handles Memory management fault.
+  */
+void MemManage_Handler(void)
+{
+	while(1)
+	{
+
+	}
+}
+
+/**
+  * @brief This function handles Pre-fetch fault, memory access fault.
+  */
+void BusFault_Handler(void)
+{
+	while(1)
+	{
+
+	}
+}
+
+/**
+  * @brief This function handles Undefined instruction or illegal state.
+  */
+void UsageFault_Handler(void)
+{
+	while(1)
+	{
+
+	}
+}
+
+/**
+  * @brief This function handles Debug monitor.
+  */
+void DebugMon_Handler(void)
+{
+
+}
+
+/******************************************************************************/
+/* STM32F4xx Peripheral Interrupt Handlers                                    */
+/* Add here the Interrupt Handlers for the used peripherals.                  */
+/* For the available peripheral interrupt handler names,                      */
+/* please refer to the startup file (startup_stm32f4xx.s).                    */
+/******************************************************************************/
+
+/**
+  * @brief This function handles TIM7 global interrupt.
+  */
+void TIM7_IRQHandler(void)
+{
+
+}
+
+/**
+  * @brief This function handles DMA1 stream0 global interrupt.
+  */
+void DMA1_Stream0_IRQHandler(void)
+{
+
+}
+
+/**
+  * @brief This function handles DMA1 stream7 global interrupt.
+  */
+void DMA1_Stream7_IRQHandler(void)
+{
+
+}
+
+/**
+  * @brief This function handles UART5 global interrupt.
+  */
+void UART5_IRQHandler(void)
+{
+
+}
