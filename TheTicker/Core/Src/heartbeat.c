7ec7a2babafd9ff57f96b64d9961ebda52b1639e
Ulad Shumeika <wladshumeiko228@gmail.com>
2023-01-15 17:28:56 +0100

    [heartbeat.c] feat: add HEARTBEAT_gpioInit function and add blink code to the heartbeat thread

--- TheTicker/Core/Src/heartbeat.c
+++ TheTicker/Core/Src/heartbeat.c
@@ -4,6 +4,18 @@
 #include "heartbeat.h"
 
 //---------------------------------------------------------------------------
+// Defines
+//---------------------------------------------------------------------------
+#define PORT_LED_HEARTBEAT				GPIOG
+#define PIN_LED_HEARTBEAT				GPIO_Pin_13
+#define BLICK_DELAY_HEARTBEAT			(700U)				// ms
+
+//---------------------------------------------------------------------------
+// Static function prototypes
+//---------------------------------------------------------------------------
+static void HEARTBEAT_gpioInit(void);
+
+//---------------------------------------------------------------------------
 // Descriptions of FreeRTOS elements
 //---------------------------------------------------------------------------
 static osThreadId heartbeatHandle;
@@ -19,20 +31,42 @@ static osThreadId heartbeatHandle;
 */
 void heartbeatTask(void const * argument)
 {
-  //HEARTBEAT_GPIO_Init();
+	HEARTBEAT_gpioInit();
 
-  /* Infinite loop */
-  for(;;)
-  {
-	//HAL_GPIO_TogglePin(PORT_LED_HEARTBEAT, PIN_LED_HEARTBEAT);
-    //osDelay(BLICK_DELAY_HEARTBEAT);
-	  osDelay(1);
-  }
+	/* Infinite loop */
+	for(;;)
+	{
+		GPIO_ToggleBits(PORT_LED_HEARTBEAT, PIN_LED_HEARTBEAT);
+		osDelay(BLICK_DELAY_HEARTBEAT);
+	}
 }
 
 //---------------------------------------------------------------------------
 // Initialization functions
 //---------------------------------------------------------------------------
+
+/**
+  * @brief  GPIO configuration for heartbeat module
+  * @param  None
+  * @retval None
+  */
+static void HEARTBEAT_gpioInit(void)
+{
+	GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+	RCC_AHB1PeriphClockCmd(RCC_AHB1ENR_GPIOGEN, ENABLE);
+
+	// Configure GPIO pins : PIN_LED_HEARTBEAT
+	GPIO_InitStruct.GPIO_Pin 			= PIN_LED_HEARTBEAT;
+	GPIO_InitStruct.GPIO_Mode			= GPIO_Mode_OUT;
+	GPIO_InitStruct.GPIO_Speed 			= GPIO_High_Speed;
+	GPIO_InitStruct.GPIO_OType 			= GPIO_OType_PP;
+	GPIO_InitStruct.GPIO_PuPd			= GPIO_PuPd_DOWN;
+	GPIO_Init(PORT_LED_HEARTBEAT, &GPIO_InitStruct);
+
+	// Configure GPIO pin Output Level
+	GPIO_ResetBits(PORT_LED_HEARTBEAT, PIN_LED_HEARTBEAT);
+}
 
 /**
   * @brief  FreeRTOS initialization for heartbeat module
