091c855494917eb762d76157656f8d3d718da01b
Ulad Shumeika <wladshumeiko228@gmail.com>
2023-01-18 23:52:15 +0100

    [custom_stm32f429_spi.h] feat: add SPI baudrate prescaler enumeration and SPI init structure definition

--- TheTicker/Drivers/Custom/Inc/custom_stm32f429_spi.h
+++ TheTicker/Drivers/Custom/Inc/custom_stm32f429_spi.h
@@ -29,26 +29,42 @@
   */
 typedef struct
 {
-	SPI_TypeDef SPIx;					//
+	SPI_TypeDef* SPIx;				// A pointer to SPIx peripheral to be used where x is between 1 to 6
 
-//	SPI_pinsPack_t pinsPack;			//
+	uint8_t pinsPack;				// SPI pinsPack enumeration to select pins combination for SPI.
+									// This parameter can be a value of @ref SPI_pinsPack_t
 
-	uint16_t SPI_BaudRatePrescaler;		//
+	uint16_t baudRatePrescaler;		// The Baud Rate prescaler value which will be used to configure the transmit
+									// and receive SCK clock. This parameter can be a value of @ref SPI_baudRatePrescaler_t
 
-//	TM_SPI_Mode_t SPI_Mode_t,       \
-//		uint16_t SPI_Mode,              \
-//		uint16_t SPI_FirstBit
+	uint16_t SPI_mode;       		// SPI modes selection. This parameter can be a value of @ref SPI_mode_t
 
 } SPI_initTypeDef;
 
 /**
+ * @brief	SPI baudrate prescaler enumeration
+ */
+typedef enum
+{
+	SPI_BaudRatePrescaler_2		= 0x0000,
+	SPI_BaudRatePrescaler_4		= 0x0008,
+	SPI_BaudRatePrescaler_8		= 0x0010,
+	SPI_BaudRatePrescaler_16	= 0x0018,
+	SPI_BaudRatePrescaler_32	= 0x0020,
+	SPI_BaudRatePrescaler_64	= 0x0028,
+	SPI_BaudRatePrescaler_128	= 0x0030,
+	SPI_BaudRatePrescaler_256	= 0x0038
+} SPI_baudRatePrescaler_t;
+
+/**
  * @brief  SPI modes selection enumeration
  */
-typedef enum {
-	SPI_Mode_0,		// clock polarity low, clock phase 1st edge
-	SPI_Mode_1, 	// clock polarity low, clock phase 2nd edge
-	SPI_Mode_2, 	// clock polarity high, clock phase 1st edge
-	SPI_Mode_3  	// clock polarity high, clock phase 2nd edge
+typedef enum
+{
+	SPI_Mode_0	= 0x0000,		// clock polarity low, clock phase 1st edge
+	SPI_Mode_1	= 0x0010, 		// clock polarity low, clock phase 2nd edge
+	SPI_Mode_2	= 0x0001, 		// clock polarity high, clock phase 1st edge
+	SPI_Mode_3  = 0x0011 		// clock polarity high, clock phase 2nd edge
 } SPI_mode_t;
 
 /**
