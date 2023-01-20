07c9c71e7bf3dfa7e8a377f3ea8815527c15f83f
Ulad Shumeika <wladshumeiko228@gmail.com>
2023-01-20 01:45:23 +0100

    [heartbeat.c] feat: change all functions from SPL library

--- TheTicker/Core/Src/heartbeat.c
+++ TheTicker/Core/Src/heartbeat.c
@@ -7,7 +7,7 @@
 // Defines
 //---------------------------------------------------------------------------
 #define PORT_LED_HEARTBEAT				GPIOG
-#define PIN_LED_HEARTBEAT				GPIO_Pin_13
+#define PIN_LED_HEARTBEAT				GPIO_PIN_13
 #define BLICK_DELAY_HEARTBEAT			(1000U)				// ms
 
 //---------------------------------------------------------------------------
@@ -36,7 +36,7 @@ void heartbeatTask(void const * argument)
 	/* Infinite loop */
 	for(;;)
 	{
-		GPIO_ToggleBits(PORT_LED_HEARTBEAT, PIN_LED_HEARTBEAT);
+		GPIO_toggleBits(PORT_LED_HEARTBEAT, PIN_LED_HEARTBEAT);
 		osDelay(BLICK_DELAY_HEARTBEAT);
 	}
 }
@@ -52,20 +52,20 @@ void heartbeatTask(void const * argument)
   */
 static void HEARTBEAT_gpioInit(void)
 {
-	GPIO_InitTypeDef GPIO_InitStruct = {0};
+	USH_GPIO_initTypeDef GPIO_InitStruct = {0};
 
 	RCC_AHB1PeriphClockCmd(RCC_AHB1ENR_GPIOGEN, ENABLE);
 
 	// Configure GPIO pins : PIN_LED_HEARTBEAT
-	GPIO_InitStruct.GPIO_Pin 			= PIN_LED_HEARTBEAT;
-	GPIO_InitStruct.GPIO_Mode			= GPIO_Mode_OUT;
-	GPIO_InitStruct.GPIO_Speed 			= GPIO_High_Speed;
-	GPIO_InitStruct.GPIO_OType 			= GPIO_OType_PP;
-	GPIO_InitStruct.GPIO_PuPd			= GPIO_PuPd_DOWN;
-	GPIO_Init(PORT_LED_HEARTBEAT, &GPIO_InitStruct);
+	GPIO_InitStruct.GPIOx				= PORT_LED_HEARTBEAT;
+	GPIO_InitStruct.Pin					= PIN_LED_HEARTBEAT;
+	GPIO_InitStruct.Mode				= GPIO_MODE_OUTPUT_PP;
+	GPIO_InitStruct.Pull				= GPIO_PULLDOWN;
+	GPIO_InitStruct.Speed				= GPIO_SPEED_VERY_HIGH;
+	GPIO_init(&GPIO_InitStruct);
 
 	// Configure GPIO pin Output Level
-	GPIO_ResetBits(PORT_LED_HEARTBEAT, PIN_LED_HEARTBEAT);
+	GPIO_resetBits(PORT_LED_HEARTBEAT, PIN_LED_HEARTBEAT);
 }
 
 /**
