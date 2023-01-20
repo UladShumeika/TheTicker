50dea05054e21462c1ed9f922dde5aabb4f63eaf
Ulad Shumeika <wladshumeiko228@gmail.com>
2023-01-20 19:32:11 +0100

    [ush_spi.h] feat: delete SPI_init finction (old version)

--- TheTicker/Drivers/Custom/Src/ush_spi.c
+++ TheTicker/Drivers/Custom/Src/ush_spi.c
@@ -71,53 +71,6 @@ void SPI_init(USH_SPI_initDefaultTypeDef *initStructure)
 //---------------------------------------------------------------------------
 
 /**
- * @brief  Initializes SPIx peripheral with custom pinspack and default other settings
- * @param  *SPIx: Pointer to SPIx peripheral you will use, where x is between 1 to 6
- * @param  pinspack: Pinspack you will use from default SPI table. This parameter can be a value of
- * 					 @ref CUS_SPI_PinsPack_t enumeration
- * @retval None
- */
-void SPI_init(SPI_initTypeDef* SPI_initStruct)
-{
-	uint16_t tmprgr, pins;
-
-	// Check the parameters
-	assert_param(IS_SPI_ALL_PERIPH(SPI_initStruct->SPIx));
-	assert_param(IS_SPI_PINSPACK(SPI_initStruct->pinsPack));
-	assert_param(IS_SPI_BAUDRATE_PRESCALER(SPI_initStruct->baudRatePrescaler));
-	assert_param(IS_SPI_MODE(SPI_initStruct->SPI_mode));
-
-	if(SPI_initStruct->SPIx == SPI1)
-	{
-		// Enable SPI1 clock
-		RCC->APB2ENR |= RCC_APB2ENR_SPI1EN;
-
-		if(SPI_initStruct->pinsPack == SPI_pinsPack_1)
-		{
-//			pins = GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7;
-//			SPI_gpioInitPins(GPIOA, pins, GPIO_AF_SPI1);
-		}
-	}
-
-
-
-//	if(SPIx == SPI1)
-//	{
-//
-//
-//		if(pinsPack == SPI_pinsPack_1)
-//		{
-//
-//		} else		// SPI_pinsPack_2
-//		{
-//			pins = GPIO_Pin_3 | GPIO_Pin_4 | GPIO_Pin_5;
-//			SPI_gpioInitPins(GPIOB, pins, GPIO_AF_SPI1);
-//		}
-
-//	}
-}
-
-/**
  * @brief	This function enables clock for the selected port
  * @param 	*GPIOx: A pointer to GPIOx peripheral to be used where x is between A to F
  * @retval	None
