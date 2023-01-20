46a12e4ab3723656e16376bd8d109f677cbbaed7
Ulad Shumeika <wladshumeiko228@gmail.com>
2023-01-20 11:59:58 +0100

    [ush_spi.h] feat: add SPI pinsPack enumeration to select pins combination for SPI

--- TheTicker/Drivers/Custom/Inc/ush_spi.h
+++ TheTicker/Drivers/Custom/Inc/ush_spi.h
@@ -38,11 +38,18 @@ typedef struct
 	uint16_t BaudRatePrescaler;		/* The Baud Rate prescaler value which will be used to configure the transmit
 									   and receive SCK clock. This parameter can be a value of @ref SPI_baudRatePrescaler_t */
 
-	uint16_t SPI_mode;       		/* SPI modes selection. This parameter can be a value of @ref SPI_mode_t */
+	uint16_t Mode;       			/* SPI modes selection. This parameter can be a value of @ref SPI_mode_t */
 
 } USH_SPI_initDefaultTypeDef;
 
-
+/**
+ * @brief SPI pinsPack enumeration to select pins combination for SPI
+ */
+typedef enum
+{
+	SPI_PINSPACK_1	= 0x00,		/* pinsPack1 selected */
+	SPI_PINSPACK_2	= 0x01		/* pinsPack2 selected */
+} USH_SPI_pinsPack;
 
 
 
@@ -81,55 +88,13 @@ typedef enum
 	SPI_Mode_3  = 0x0011 		// clock polarity high, clock phase 2nd edge
 } SPI_mode_t;
 
-/**
- * @brief  SPI pinsPack enumeration to select pins combination for SPI
- */
-typedef enum 
-{
-	SPI_pinsPack_1	= 0x00,		// select PinsPack1 from Pinout table for specific SPI
-	SPI_pinsPack_2	= 0x01		// select PinsPack2 from Pinout table for specific SPI
-} SPI_pinsPack_t;
 
-/** 
-  * @brief  GPIO configuration mode enumeration 
-  */   
-typedef enum
-{ 
-	SPI_GPIO_Mode_IN	= 0x00,		// GPIO input mode
-	SPI_GPIO_Mode_OUT	= 0x01,  	// GPIO output mode
-	SPI_GPIO_Mode_AF	= 0x02,  	// GPIO alternate function mode
-	SPI_GPIO_Mode_AN	= 0x03   	// GPIO analog mode
-} SPI_GPIOMode_TypeDef;
 
-/** 
-  * @brief  GPIO output maximum frequency enumeration
-  */  
-typedef enum
-{ 
-	SPI_GPIO_Low_Speed		= 0x00,		// low speed
-	SPI_GPIO_Medium_Speed	= 0x01, 	// medium speed
-	SPI_GPIO_Fast_Speed		= 0x02, 	// fast speed
-	SPI_GPIO_High_Speed		= 0x03  	// high speed
-} SPI_GPIOSpeed_TypeDef;
 
-/**
-  * @brief  GPIO output type enumeration
-  */
-typedef enum
-{
-	SPI_GPIO_OType_PP	= 0x00,		// push-pull
-	SPI_GPIO_OType_OD 	= 0x01		// open-drain
-} SPI_GPIOOType_TypeDef;
 
-/**
-  * @brief  GPIO configuration pull-up pull-down enumeration
-  */
-typedef enum
-{
-	SPI_GPIO_PuPd_NOPULL 	= 0x00,		// no pull
-	SPI_GPIO_PuPd_UP		= 0x01,		// pull-up
-	SPI_GPIO_PuPd_DOWN		= 0x02		// pull-down
-} SPI_GPIOPuPd_TypeDef;
+
+
+
 
 //---------------------------------------------------------------------------
 // External function prototypes
