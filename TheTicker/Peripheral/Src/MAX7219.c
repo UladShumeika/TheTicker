3694ba94f74defcee2239e8987220da700074735
Ulad Shumeika <wladshumeiko228@gmail.com>
2023-01-23 15:52:06 +0100

    [MAX7219.c] feat: add MAX7219_init function

--- TheTicker/Peripheral/Src/MAX7219.c
+++ TheTicker/Peripheral/Src/MAX7219.c
@@ -1,8 +1,26 @@
 //---------------------------------------------------------------------------
 // Includes
 //---------------------------------------------------------------------------
-#include <ush_spi.h>
 #include "MAX7219.h"
+
+//---------------------------------------------------------------------------
+// Initialization functions
+//---------------------------------------------------------------------------
+
+/**
+ * @brief	This function initializes MAX7219.
+ * @retval	None.
+ */
+void MAX7219_init(void)
+{
+	USH_SPI_initDefaultTypeDef initStructure = {0,};
+
+	initStructure.SPIx 					= SPI1;
+	initStructure.PinsPack 				= SPI_PINSPACK_1;
+	initStructure.BaudRatePrescaler		= SPI_BAUDRATE_PRESCALER_16;
+	initStructure.Mode 					= SPI_MODE_1;
+	SPI_init(&initStructure);
+}
 
 //---------------------------------------------------------------------------
 // Library Functions
