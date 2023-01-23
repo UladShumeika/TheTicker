1c0256fc78bb80a77411af6d20f0cb71d1fb8299
Ulad Shumeika <wladshumeiko228@gmail.com>
2023-01-23 16:00:08 +0100

    [MAX7219.c] feat: add MAX7219_state function

--- TheTicker/Peripheral/Src/MAX7219.c
+++ TheTicker/Peripheral/Src/MAX7219.c
@@ -27,6 +27,24 @@ void MAX7219_init(void)
 //---------------------------------------------------------------------------
 
 /**
+  * @brief  This function enables or disables a matrix controller.
+  * @param  digit - The digit indicates which digit of the matrix to transfer data to.
+  * 				This parameter can be any value of @ref USH_MAX7219_digits.
+  * @param  mode - The driver mode. This parameter can be any value of @ref USH_MAX7219_REG_SHUTDOWN.
+  * @retval None.
+  */
+void MAX7219_state(uint8_t digit, USH_MAX7219_REG_SHUTDOWN mode)
+{
+	if(mode == NORMAL_MODE)
+	{
+		MAX7219_sendDataWithLatch(digit, REG_SHUTDOWN, NORMAL_MODE);
+	} else
+	{
+		MAX7219_sendDataWithLatch(digit, REG_SHUTDOWN, SHUTDOWN_MODE);
+	}
+}
+
+/**
   * @brief  This function sends data WITHOUT a latch.
   * @param	digit - The digit indicates which digit of the matrix to transfer data to.
   * 		        This parameter can be any value of @ref USH_MAX7219_digits.
@@ -69,15 +87,4 @@ void MAX7219_sendDataWithLatch(uint8_t digit, uint8_t addr, uint8_t data)
 	SPI_csPin(LOW);
 	MAX7219_sendDataWithoutLatch(digit, addr, data);
 	SPI_csPin(HIGH);
-}
-
-void MAX7219_init(void)
-{
-	USH_SPI_initDefaultTypeDef initStructure = {0,};
-
-	initStructure.SPIx = SPI1;
-	initStructure.PinsPack = SPI_PINSPACK_1;
-	initStructure.BaudRatePrescaler = SPI_BAUDRATE_PRESCALER_16;
-	initStructure.Mode = SPI_MODE_1;
-	SPI_init(&initStructure);
 }
