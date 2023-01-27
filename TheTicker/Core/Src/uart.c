477a6358dae7e5d8c94306587f0430ed8f8f3e8d
Ulad Shumeika <wladshumeiko228@gmail.com>
2023-01-27 20:21:27 +0100

    [uart.c] feat: add receivingMessageTask thread

--- TheTicker/Core/Src/uart.c
+++ TheTicker/Core/Src/uart.c
@@ -6,10 +6,25 @@
 //---------------------------------------------------------------------------
 // Descriptions of FreeRTOS elements
 //---------------------------------------------------------------------------
+static osThreadId receivingMessageHandle;
 
 //---------------------------------------------------------------------------
 // FreeRTOS's threads
 //---------------------------------------------------------------------------
+
+/**
+ * @brief 	Function implementing receiving a message thread.
+ * @param 	argument - Not used.
+ * @retval  None.
+ */
+void receivingMessageTask(void const *argument)
+{
+	/* Infinite loop */
+	for(;;)
+	{
+		osDelay(1000);
+	}
+}
 
 //---------------------------------------------------------------------------
 // Initialization functions
@@ -22,5 +37,8 @@
   */
 void UART_freeRtosInit(void)
 {
-
+	// Create the thread(s)
+	// definition and creation of receivingMessageTask
+	osThreadDef(receivingMessage, receivingMessageTask, osPriorityLow, 0, 128);
+	receivingMessageHandle = osThreadCreate(osThread(receivingMessage), NULL);
 }
