26a619afc3fca836dde7f3044bbc920c49017ff1
Ulad Shumeika <wladshumeiko228@gmail.com>
2023-01-28 20:13:37 +0100

    [MAX7219.h] feat: change MAX7219_decodeMode prototype

--- TheTicker/Peripheral/Inc/MAX7219.h
+++ TheTicker/Peripheral/Inc/MAX7219.h
@@ -174,12 +174,12 @@ void MAX7219_intensity(USH_MAX7219_digits numDigit, USH_MAX7219_REG_INTENSITY in
 
 /**
  * @brief	This function sets the decoding mode for the matrix driver
- * @param	digit - The digit indicates which digit of the matrix driver to transfer data to.
- * 					This parameter can be any value of @ref USH_MAX7219_digits.
+ * @param	numDigit - The digit indicates which digit of the matrix driver to transfer data to.
+ * 					   This parameter can be any value of @ref USH_MAX7219_digits.
  * @param	mode - The decode mode. This parameter can be any value of @ref USH_MAX7219_REG_DECODE_MODE.
  * @retval	None.
  */
-void MAX7219_decodeMode(USH_MAX7219_digits digit, USH_MAX7219_REG_DECODE_MODE mode);
+void MAX7219_decodeMode(USH_MAX7219_digits numDigit, USH_MAX7219_REG_DECODE_MODE mode);
 
 /**
   * @brief  This function enables or disables a matrix driver.
