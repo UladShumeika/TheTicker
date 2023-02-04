72e5bc0419e3db0e5342e6579a27ac89b4a320ae
Ulad Shumeika <wladshumeiko228@gmail.com>
2023-02-04 20:21:55 +0100

    [ush_dma.h] feat: add  DMA channels enumeration

--- TheTicker/Drivers/Custom/Inc/ush_dma.h
+++ TheTicker/Drivers/Custom/Inc/ush_dma.h
@@ -25,7 +25,8 @@ typedef struct
 {
 	DMA_Stream_TypeDef* DMAx;				/* A pointer to DMAx peripheral to be used where x is 1 or 2 */
 
-	uint32_t Channel;
+	uint32_t Channel;						/* A channel to be used for the specified stream */
+
 	uint32_t Direction;
 	uint32_t PeriphInc;
 	uint32_t MemInc;
@@ -38,6 +39,19 @@ typedef struct
 	uint32_t PeriphBurst;
 } USH_DMA_initTypeDef;
 
-
+/**
+ * @brief DMA channels enumeration to select a channel for DMA
+ */
+typedef enum
+{
+	DMA_CHANNEL_0	= 0x00000000U,		/* DMA Channel 0 */
+	DMA_CHANNEL_1	= 0x02000000U,		/* DMA Channel 1 */
+	DMA_CHANNEL_2	= 0x04000000U,		/* DMA Channel 2 */
+	DMA_CHANNEL_3	= 0x06000000U,		/* DMA Channel 3 */
+	DMA_CHANNEL_4 	= 0x08000000U,		/* DMA Channel 4 */
+	DMA_CHANNEL_5	= 0x0A000000U,		/* DMA Channel 5 */
+	DMA_CHANNEL_6	= 0x0C000000U,		/* DMA Channel 6 */
+	DMA_CHANNEL_7	= 0x0E000000U		/* DMA Channel 7 */
+} USH_DMA_channels;
 
 #endif /* __USH_DMA_H */
