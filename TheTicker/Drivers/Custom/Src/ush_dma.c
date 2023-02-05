8c5b1ee69473fb6e418fafcfb6bc97e1f8f4f7ca
Ulad Shumeika <wladshumeiko228@gmail.com>
2023-02-05 13:02:11 +0100

    [ush_dma.c] feat: add the empty DMA_init function

---
+++ TheTicker/Drivers/Custom/Src/ush_dma.c
@@ -0,0 +1,31 @@
+/**
+  ******************************************************************************
+  * @file    ush_dma.c
+  * @author  Ulad Shumeika
+  * @version v1.0
+  * @date    05-February-2023
+  * @brief   DMA module driver
+  ******************************************************************************
+  */
+
+//---------------------------------------------------------------------------
+// Includes
+//---------------------------------------------------------------------------
+#include "ush_dma.h"
+
+//---------------------------------------------------------------------------
+// Initialization functions
+//---------------------------------------------------------------------------
+
+/**
+ * @brief 	This function initializes the DMAx peripheral according to the specified parameters in the USH_DMA_initTypeDef.
+ * @param 	initStructure - A pointer to a USH_DMA_initTypeDef structure that contains the configuration
+ * 							information for the specified DMA peripheral.
+ * @retval	None.
+ */
+void DMA_init(USH_DMA_initTypeDef *initStructure)
+{
+	uint32_t temp = 0;
+
+
+}
