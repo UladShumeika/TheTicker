70f2a6c12120c4e106d312a8d8bb80802eea2091
Ulad Shumeika <wladshumeiko228@gmail.com>
2023-02-05 13:01:25 +0100

    [ush_dma.h] feat: add DMA_init function's prototype

--- TheTicker/Drivers/Custom/Inc/ush_dma.h
+++ TheTicker/Drivers/Custom/Inc/ush_dma.h
@@ -199,4 +199,16 @@ typedef enum
 	DMA_FIFO_THRESHOLD_FULL			= 0x03U		/* FIFO threshold full configuration */
 } USH_DMA_fifoThresholdLevel;
 
+//---------------------------------------------------------------------------
+// External function prototypes
+//---------------------------------------------------------------------------
+
+/**
+ * @brief 	This function initializes the DMAx peripheral according to the specified parameters in the USH_DMA_initTypeDef.
+ * @param 	initStructure - A pointer to a USH_DMA_initTypeDef structure that contains the configuration
+ * 							information for the specified DMA peripheral.
+ * @retval	None.
+ */
+void DMA_init(USH_DMA_initTypeDef *initStructure);
+
 #endif /* __USH_DMA_H */
