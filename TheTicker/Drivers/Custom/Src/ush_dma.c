b5c6a44c708189bb1e1886441aad20bfe79b0fca
Ulad Shumeika <wladshumeiko228@gmail.com>
2023-02-05 19:45:22 +0100

    [ush_dma.c] feat: add DMA_state function

--- TheTicker/Drivers/Custom/Src/ush_dma.c
+++ TheTicker/Drivers/Custom/Src/ush_dma.c
@@ -29,3 +29,24 @@ void DMA_init(USH_DMA_initTypeDef *initStructure)
 
 
 }
+
+//---------------------------------------------------------------------------
+// Library Functions
+//---------------------------------------------------------------------------
+
+/**
+ * @brief 	This function enables and disables the selected DMA stream.
+ * @param 	DMAx_Streamy - A pointer to a DMA_Stream_TypeDef structure that contains the selected DMA stream.
+ * @param 	state - The state of the selected stream. This parameter can be a value of @ref FunctionalState.
+ * @retval	None.
+ */
+void DMA_state(DMA_Stream_TypeDef *DMAx_Streamy, FunctionalState state)
+{
+	if(state == ENABLE)
+	{
+		DMAx_Streamy->CR |= DMA_SxCR_EN;
+	} else
+	{
+		DMAx_Streamy->CR &= ~DMA_SxCR_EN;
+	}
+}
