f02515fdd25672f29432710d5510963c929933c5
Ulad Shumeika <wladshumeiko228@gmail.com>
2023-02-21 19:05:33 +0100

    [ush_stm32f4xx_uart.h/ush_stm32f4xx_uart.c] feat: add uart driver module

---
+++ TheTicker/Drivers/Custom/Inc/ush_stm32f4xx_uart.h
@@ -0,0 +1,30 @@
+/**
+  ******************************************************************************
+  * @file    ush_stm32f4xx_uart.h
+  * @author  Ulad Shumeika
+  * @version v1.0
+  * @date    21-February-2023
+  * @brief   This file contains the functions prototypes for the UART firmware
+  *          library.
+  *
+  * NOTE: This file is not a full-fledged UART driver, but contains only some of
+  * 	  the functions that are needed for the current project.
+  ******************************************************************************
+  */
+
+//---------------------------------------------------------------------------
+// Define to prevent recursive inclusion
+//---------------------------------------------------------------------------
+#ifndef __USH_STM32F4XX_UART_H
+#define __USH_STM32F4XX_UART_H
+
+//---------------------------------------------------------------------------
+// Includes
+//---------------------------------------------------------------------------
+#include "stm32f4xx.h"
+
+//---------------------------------------------------------------------------
+// External function prototypes
+//---------------------------------------------------------------------------
+
+#endif /* __USH_STM32F4XX_UART_H */
