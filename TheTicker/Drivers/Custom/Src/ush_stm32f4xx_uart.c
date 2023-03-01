16303582ab75d94b584b3c843c00bb21cbbeb2b3
Ulad Shumeika <wladshumeiko228@gmail.com>
2023-03-01 18:20:23 +0100

    [ush_stm32f4xx_uart.c] feat: add USH_USART_streamAndChannelTypeDef structure declaration and streamAndChannel structure

--- TheTicker/Drivers/Custom/Src/ush_stm32f4xx_uart.c
+++ TheTicker/Drivers/Custom/Src/ush_stm32f4xx_uart.c
@@ -24,6 +24,26 @@
 #define IS_UART_BAUDRATE(BAUDRATE) ((BAUDRATE) <= 10500000U)
 
 //---------------------------------------------------------------------------
+// Private structure
+//---------------------------------------------------------------------------
+
+/**
+ * @brief DMA stream and channel structure definition
+ */
+typedef struct
+{
+	DMA_Stream_TypeDef *DMAy_Streamx;	/* A pointer to Stream peripheral to be used where y is 1 or 2 and x is from 0 to 7. */
+
+	USH_DMA_channels Channel;			/* A channel to be used for the specified stream. */
+
+} USH_USART_streamAndChannelTypeDef;
+
+//---------------------------------------------------------------------------
+// Private variables
+//---------------------------------------------------------------------------
+USH_USART_streamAndChannelTypeDef streamAndChannel = {0,};
+
+//---------------------------------------------------------------------------
 // Static function prototypes
 //---------------------------------------------------------------------------
 static uint16_t USART_BRRSampling16(uint32_t pclk, uint32_t bautrate);
