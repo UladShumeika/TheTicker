fe69bf5d1f6069fd09611e5f31204173c73279a4
Ulad Shumeika <wladshumeiko228@gmail.com>
2023-03-03 11:21:41 +0100

    [ush_stm32f4xx_misc.h] feat: add MISC_NVIC_SetPriority function prototype

--- TheTicker/Drivers/Custom/Inc/ush_stm32f4xx_misc.h
+++ TheTicker/Drivers/Custom/Inc/ush_stm32f4xx_misc.h
@@ -34,4 +34,23 @@ typedef enum
 	STATUS_OK	 	= !STATUS_BUSY		/* Periphery status ok */
 } USH_peripheryStatus;
 
+//---------------------------------------------------------------------------
+// External function prototypes
+//---------------------------------------------------------------------------
+
+/**
+  * @brief  This function sets the priority of an interrupt.
+  * @param  IRQn External interrupt number.
+  *         This parameter can be an enumerator of IRQn_Type enumeration
+  *         (For the complete STM32 Devices IRQ Channels list, please refer to the appropriate CMSIS device file (stm32f4xxxx.h))
+  * @param  PreemptPriority - The preemption priority for the IRQn channel.
+  *         This parameter can be a value between 0 and 15.
+  *         A lower priority value indicates a higher priority.
+  * @param  SubPriority - The subpriority level for the IRQ channel.
+  *         This parameter can be a value between 0 and 15.
+  *         A lower priority value indicates a higher priority.
+  * @retval	None.
+  */
+void MISC_NVIC_SetPriority(IRQn_Type IRQn, uint32_t preemptPriority, uint32_t subPriority);
+
 #endif /* __USH_STM32F4XX_MISC_H */
