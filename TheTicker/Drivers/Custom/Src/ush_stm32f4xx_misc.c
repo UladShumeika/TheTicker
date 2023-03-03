4d24080300ffcdb325fdee773141c693949132f5
Ulad Shumeika <wladshumeiko228@gmail.com>
2023-03-03 11:22:33 +0100

    [ush_stm32f4xx_misc.c] feat: add MISC_NVIC_SetPriority function and IS_NVIC_SUB_PRIORITY and IS_NVIC_PREEMPTION_PRIORITY macros

--- TheTicker/Drivers/Custom/Src/ush_stm32f4xx_misc.c
+++ TheTicker/Drivers/Custom/Src/ush_stm32f4xx_misc.c
@@ -15,3 +15,38 @@
 //---------------------------------------------------------------------------
 #include "ush_stm32f4xx_misc.h"
 
+//---------------------------------------------------------------------------
+// Macros
+//---------------------------------------------------------------------------
+#define IS_NVIC_SUB_PRIORITY(PRIORITY)  		((PRIORITY) < 16U)
+#define IS_NVIC_PREEMPTION_PRIORITY(PRIORITY)  	((PRIORITY) < 16U)
+
+//---------------------------------------------------------------------------
+// Library Functions
+//---------------------------------------------------------------------------
+
+/**
+  * @brief  This function sets the priority of an interrupt.
+  * @param  IRQn External interrupt number.
+  *         This parameter can be an enumerator of IRQn_Type enumeration
+  *         (For the complete STM32 Devices IRQ Channels list, please refer to the appropriate CMSIS device file (stm32f4xxxx.h))
+  * @param  PreemptPriority - The preemption priority for the IRQn channel.
+  *         This parameter can be a value between 0 and 15.
+  *         A lower priority value indicates a higher priority.
+  * @param  SubPriority - The subpriority level for the IRQ channel.
+  *         This parameter can be a value between 0 and 15.
+  *         A lower priority value indicates a higher priority.
+  * @retval	None.
+  */
+void MISC_NVIC_SetPriority(IRQn_Type IRQn, uint32_t preemptPriority, uint32_t subPriority)
+{
+  uint32_t prioritygroup = 0x00U;
+
+  // Check the parameters
+  assert_param(IS_NVIC_SUB_PRIORITY(subPriority));
+  assert_param(IS_NVIC_PREEMPTION_PRIORITY(preemptPriority));
+
+  prioritygroup = NVIC_GetPriorityGrouping();
+
+  NVIC_SetPriority(IRQn, NVIC_EncodePriority(prioritygroup, preemptPriority, subPriority));
+}
