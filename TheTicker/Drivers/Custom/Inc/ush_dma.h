5fb87055c7207d26fe56d4b7724743c408b1a239
Ulad Shumeika <wladshumeiko228@gmail.com>
2023-03-06 19:54:35 +0100

    [ush_dma.h] feat: add DMA_transferErrorCallback function prototype

--- TheTicker/Drivers/Custom/Inc/ush_dma.h
+++ TheTicker/Drivers/Custom/Inc/ush_dma.h
@@ -342,5 +342,6 @@ void DMA_IRQHandler(USH_DMA_initTypeDef *initStructure);
 //---------------------------------------------------------------------------
 __WEAK void DMA_transferCompleteCallback(DMA_Stream_TypeDef *DMAy_Streamx);
 __WEAK void DMA_halfTransferCompleteCallback(DMA_Stream_TypeDef *DMAy_Streamx);
+__WEAK void DMA_transferErrorCallback(DMA_Stream_TypeDef *DMAy_Streamx);
 
 #endif /* __USH_DMA_H */
