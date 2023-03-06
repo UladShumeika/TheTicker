ee2f42a1d36aff2f03eee322bf87d240f16e7f87
Ulad Shumeika <wladshumeiko228@gmail.com>
2023-03-06 12:45:09 +0100

    [ush_spi.h] test: fix the error in OR operation

--- TheTicker/Drivers/Custom/Inc/ush_spi.h
+++ TheTicker/Drivers/Custom/Inc/ush_spi.h
@@ -143,31 +143,31 @@ typedef enum
 // Macros
 //---------------------------------------------------------------------------
 
-#define IS_SPI_ALL_INSTANCE(INSTANCE)			(((INSTANCE) == SPI1) | \
-												 ((INSTANCE) == SPI2) | \
-												 ((INSTANCE) == SPI3) | \
-												 ((INSTANCE) == SPI4) | \
-												 ((INSTANCE) == SPI5) | \
+#define IS_SPI_ALL_INSTANCE(INSTANCE)			(((INSTANCE) == SPI1) || \
+												 ((INSTANCE) == SPI2) || \
+												 ((INSTANCE) == SPI3) || \
+												 ((INSTANCE) == SPI4) || \
+												 ((INSTANCE) == SPI5) || \
 												 ((INSTANCE) == SPI6))
 
-#define IS_SPI_PINSPACK(PINSPACK)				(((PINSPACK) == SPI_PINSPACK_1) | \
+#define IS_SPI_PINSPACK(PINSPACK)				(((PINSPACK) == SPI_PINSPACK_1) || \
 										 	 	 ((PINSPACK) == SPI_PINSPACK_2))
 
-#define IS_SPI_BAUDRATE_PRESCALER(PRESCALER)	(((PRESCALER) == SPI_BAUDRATE_PRESCALER_2)   | \
-												 ((PRESCALER) == SPI_BAUDRATE_PRESCALER_4)   | \
-												 ((PRESCALER) == SPI_BAUDRATE_PRESCALER_8)   | \
-												 ((PRESCALER) == SPI_BAUDRATE_PRESCALER_16)  | \
-												 ((PRESCALER) == SPI_BAUDRATE_PRESCALER_32)  | \
-												 ((PRESCALER) == SPI_BAUDRATE_PRESCALER_64)  | \
-												 ((PRESCALER) == SPI_BAUDRATE_PRESCALER_128) | \
+#define IS_SPI_BAUDRATE_PRESCALER(PRESCALER)	(((PRESCALER) == SPI_BAUDRATE_PRESCALER_2)   || \
+												 ((PRESCALER) == SPI_BAUDRATE_PRESCALER_4)   || \
+												 ((PRESCALER) == SPI_BAUDRATE_PRESCALER_8)   || \
+												 ((PRESCALER) == SPI_BAUDRATE_PRESCALER_16)  || \
+												 ((PRESCALER) == SPI_BAUDRATE_PRESCALER_32)  || \
+												 ((PRESCALER) == SPI_BAUDRATE_PRESCALER_64)  || \
+												 ((PRESCALER) == SPI_BAUDRATE_PRESCALER_128) || \
 												 ((PRESCALER) == SPI_BAUDRATE_PRESCALER_256))
 
-#define IS_SPI_MODE(MODE)						(((MODE) == SPI_MODE_1) | \
-												 ((MODE) == SPI_MODE_2) | \
-												 ((MODE) == SPI_MODE_3) | \
+#define IS_SPI_MODE(MODE)						(((MODE) == SPI_MODE_1) || \
+												 ((MODE) == SPI_MODE_2) || \
+												 ((MODE) == SPI_MODE_3) || \
 												 ((MODE) == SPI_MODE_4))
 
-#define IS_SPI_PIN_STATE(STATE)					(((STATE) == LOW) | \
+#define IS_SPI_PIN_STATE(STATE)					(((STATE) == LOW) || \
 												 ((STATE) == HIGH))
 
 //---------------------------------------------------------------------------
