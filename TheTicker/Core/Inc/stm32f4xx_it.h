a436255e52a7ffd77f8f3ba6570f9225216600de
Ulad Shumeika <wladshumeiko228@gmail.com>
2023-03-07 22:57:00 +0100

    [stm32f4xx_it.h] feat: add USART1_IRQHandler function prototype

--- TheTicker/Core/Inc/stm32f4xx_it.h
+++ TheTicker/Core/Inc/stm32f4xx_it.h
@@ -19,6 +19,7 @@ void UsageFault_Handler(void);
 void DebugMon_Handler(void);
 void DMA2_Stream7_IRQHandler(void);
 void DMA2_Stream2_IRQHandler(void);
+void USART1_IRQHandler(void);
 
 #ifdef __cplusplus
 }
