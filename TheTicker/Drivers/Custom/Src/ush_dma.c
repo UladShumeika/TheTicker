8f71331f07ea127a06d509d9fb9b73efbc3e37af
Ulad Shumeika <wladshumeiko228@gmail.com>
2023-03-12 18:39:48 +0100

    [ush_dma.c] docs: change description

--- TheTicker/Drivers/Custom/Src/ush_dma.c
+++ TheTicker/Drivers/Custom/Src/ush_dma.c
@@ -3,8 +3,11 @@
   * @file    ush_dma.c
   * @author  Ulad Shumeika
   * @version v1.0
-  * @date    05-February-2023
-  * @brief   DMA module driver
+  * @date    5-January-2023
+  * @brief	 This file contains the implementation of functions for working with DMA.
+  *
+  *	NOTE: This file is not a full-fledged DMA driver, but contains only some of
+  * 	  the functions that are needed for the current project.
   ******************************************************************************
   */
 
