dedede34053391b7e065aaeb07c9586f3b3db177
Ulad Shumeika <wladshumeiko228@gmail.com>
2023-03-13 11:50:33 +0100

    [ush_stm32f4xx_misc.c] feat: add MISC_FLASH_setLatency function

--- TheTicker/Drivers/Custom/Src/ush_stm32f4xx_misc.c
+++ TheTicker/Drivers/Custom/Src/ush_stm32f4xx_misc.c
@@ -162,6 +162,28 @@ void MISC_FLASH_dataCacheCmd(FunctionalState newState)
   }
 }
 
+/**
+  * @brief  This function sets the code latency value.
+  * @param  flashLatency - specifies the FLASH Latency value.
+  *          			   This parameter can be a value of @ref USH_FLASH_latency.
+  *
+  * @note For STM32F405xx/407xx, STM32F415xx/417xx, STM32F401xx/411xE/STM32F412xG and STM32F413_423xx devices
+  *       this parameter can be a value between FLASH_LATENCY_0 and FLASH_LATENCY_7.
+  *
+  * @note For STM32F42xxx/43xxx devices this parameter can be a value between
+  *       FLASH_LATENCY_0 and FLASH_LATENCY_15.
+  *
+  * @retval None
+  */
+void MISC_FLASH_setLatency(USH_FLASH_latency flashLatency)
+{
+  // Check the parameters
+  assert_param(IS_MISC_FLASH_LATENCY(flashLatency));
+
+  // Perform Byte access to FLASH_ACR[8:0] to set the Latency value
+  *(__IO uint8_t *)FLASH_ACR_BYTE0_ADDRESS = (uint8_t)flashLatency;
+}
+
 //---------------------------------------------------------------------------
 // Function's parameters check.
 //---------------------------------------------------------------------------
