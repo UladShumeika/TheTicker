2e39c0f5ffecd867760ee93e57502666a11902ba
Ulad Shumeika <wladshumeiko228@gmail.com>
2023-04-11 18:55:31 +0200

    [heartbeat.h/heartbeat.c] feat: add HEARTBEAT_freeRtosInit function and HeartbeatTask thread

--- TheTicker/Core/Src/heartbeat.c
+++ TheTicker/Core/Src/heartbeat.c
@@ -2,3 +2,47 @@
 // Includes
 //---------------------------------------------------------------------------
 #include "heartbeat.h"
+
+//---------------------------------------------------------------------------
+// Descriptions of FreeRTOS elements
+//---------------------------------------------------------------------------
+static osThreadId HeartbeatHandle;
+
+//---------------------------------------------------------------------------
+// FreeRTOS's threads
+//---------------------------------------------------------------------------
+
+/**
+* @brief Function implementing the heartbeat thread.
+* @param argument: Not used
+* @retval None
+*/
+void HeartbeatTask(void const * argument)
+{
+  //HEARTBEAT_GPIO_Init();
+
+  /* Infinite loop */
+  for(;;)
+  {
+	//HAL_GPIO_TogglePin(PORT_LED_HEARTBEAT, PIN_LED_HEARTBEAT);
+    //osDelay(BLICK_DELAY_HEARTBEAT);
+	  osDelay(1);
+  }
+}
+
+//---------------------------------------------------------------------------
+// Initialization functions
+//---------------------------------------------------------------------------
+
+/**
+  * @brief  FreeRTOS initialization for heartbeat module
+  * @param  None
+  * @retval None
+  */
+void HEARTBEAT_freeRtosInit(void)
+{
+	// Create the thread(s)
+	// definition and creation of HeartbeatTask
+	osThreadDef(Heartbeat, HeartbeatTask, osPriorityLow, 0, 128);
+	HeartbeatHandle = osThreadCreate(osThread(Heartbeat), NULL);
+}
