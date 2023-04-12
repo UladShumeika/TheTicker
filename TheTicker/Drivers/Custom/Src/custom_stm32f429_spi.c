39c57a099b6b39539edb55dbad84e0e373884ba2
Ulad Shumeika <wladshumeiko228@gmail.com>
2023-04-12 23:28:31 +0200

    [_spi.c] feat: add SPI_gpioInitPins function [_spi.c] fix: solve the merge problem

--- TheTicker/Drivers/Custom/Src/custom_stm32f429_spi.c
+++ TheTicker/Drivers/Custom/Src/custom_stm32f429_spi.c
@@ -15,28 +15,67 @@
 #include "custom_stm32f429_spi.h"
 
 //---------------------------------------------------------------------------
+// Static function prototypes
+//---------------------------------------------------------------------------
+static void SPI_gpioInitPins(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin);
+
+//---------------------------------------------------------------------------
 // Library Functions
 //---------------------------------------------------------------------------
 
-static void SPI_gpioInit(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin)
+/**
+ * @brief  Initializes SPIx peripheral with custom pinspack and default other settings
+ * @param  *SPIx: Pointer to SPIx peripheral you will use, where x is between 1 to 6
+ * @param  pinspack: Pinspack you will use from default SPI table. This parameter can be a value of
+ * 					 @ref CUS_SPI_PinsPack_t enumeration
+ * @retval None
+ */
+// void CUS_spiInit(SPI_TypeDef* SPIx, CUS_SPI_PinsPack_t pinspack)
+// {
+
+// }
+
+/**
+ * @brief	This function initializes the GPIO peripheral to be used with the selected SPI
+ * @note	Default GPIO parameters:
+ * 			Function mode - alternate function mode;
+ * 			Speed mode - High speed mode;
+ * 			Output mode - Output push-pull;
+ * 			Resistor configuration - No pull
+ * @param	GPIOx: A pointer to GPIOx peripheral to be used where x is between A to F
+ * @param	GPIO_Pin: Specifies the GPIO pins to be configured. This parameter can be any value of 
+ * 					  @ref GPIO_pins_define
+ * @retval	None
+*/
+static void SPI_gpioInitPins(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin)
 {
-	GPIO_InitTypeDef GPIO_InitStruct = {0};
+	uint32_t pinPos = 0x00, pos = 0x00 , currentPin = 0x00;
 
-#ifdef USE_SPI1
-	if(GPIOx == SPI1)
-	{
-		RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+	// Check the parameters
 
-		GPIO_InitStruct.GPIO_Pin 			= GPIO_Pin;
-		GPIO_InitStruct.GPIO_Mode			= GPIO_Mode_OUT;
-		GPIO_InitStruct.GPIO_Speed 			= GPIO_High_Speed;
-		GPIO_InitStruct.GPIO_OType 			= GPIO_OType_PP;
-		GPIO_InitStruct.GPIO_PuPd			= GPIO_PuPd_NOPULL;
-		GPIO_Init(GPIOx, &GPIO_InitStruct);
+	for (pinPos = 0x00; pinPos < 0x10; pinPos++) // 0x10 -> 16 ()
+	{	
+		// Get the port pins position 
+		pos = ((uint32_t)0x01) << pinPos;
+		currentPin = GPIO_Pin & pos;
+
+		if(currentPin == pos)
+		{	
+			// Enable alternate function mode
+			GPIOx->MODER  &= ~(GPIO_MODER_MODER0 << (pinPos * 2));
+      		GPIOx->MODER |= (((uint32_t)SPI_GPIO_Mode_AF) << (pinPos * 2));
+
+        	// Speed mode configuration
+        	GPIOx->OSPEEDR &= ~(GPIO_OSPEEDER_OSPEEDR0 << (pinPos * 2));
+        	GPIOx->OSPEEDR |= ((uint32_t)(SPI_GPIO_High_Speed) << (pinPos * 2));
+
+        	// Output mode configuration
+        	GPIOx->OTYPER  &= ~((GPIO_OTYPER_OT_0) << ((uint16_t)pinPos)) ;
+        	GPIOx->OTYPER |= (uint16_t)(((uint16_t)SPI_GPIO_OType_PP) << ((uint16_t)pinPos));
+
+			// Pull-up pull-down resistor configuration
+      		GPIOx->PUPDR &= ~(GPIO_PUPDR_PUPDR0 << ((uint16_t)pinPos * 2));
+      		//GPIOx->PUPDR |= (((uint32_t)SPI_GPIO_PuPd_NOPULL) << (pinPos * 2));
+		}
 	}
-#endif
-
-
-
 }
-
