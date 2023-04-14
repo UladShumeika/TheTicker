b1ad6780b8894ff2d856d5e2c4f0f8907fac9706
Ulad Shumeika <wladshumeiko228@gmail.com>
2023-04-14 15:25:48 +0200

    [_misc.h] feat: add USH_NVIC_priorityGroup enum

--- TheTicker/Drivers/Custom/Inc/ush_stm32f4xx_misc.h
+++ TheTicker/Drivers/Custom/Inc/ush_stm32f4xx_misc.h
@@ -52,6 +52,27 @@ typedef enum
 	FLASH_LATENCY_15,		/* Flash fifteen latency cycle */
 } USH_FLASH_latency;
 
+/**
+ * @brief Preemption priority group enumeration
+ */
+typedef enum
+{
+	NVIC_PRIORITYGROUP_0	= 0x00000007U,		/* 0 bits for pre-emption priority
+	 	 	 	 	 	 	 	 	 	 	 	   4 bits for subpriority */
+
+	NVIC_PRIORITYGROUP_1	= 0x00000006U,		/* 1 bits for pre-emption priority
+												   3 bits for subpriority */
+
+	NVIC_PRIORITYGROUP_2	= 0x00000005U,		/* 2 bits for pre-emption priority
+												   2 bits for subpriority */
+
+	NVIC_PRIORITYGROUP_3	= 0x00000004U,		/* 3 bits for pre-emption priority
+												   1 bits for subpriority */
+
+	NVIC_PRIORITYGROUP_4	= 0x00000003U,		/* 4 bits for pre-emption priority
+												   0 bits for subpriority */
+} USH_NVIC_priorityGroup;
+
 //---------------------------------------------------------------------------
 // Macros
 //---------------------------------------------------------------------------
