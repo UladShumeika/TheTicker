189e15fb56dd7ce40a43cf64eae1e8552f7dcfe2
Ulad Shumeika <wladshumeiko228@gmail.com>
2023-04-14 17:41:30 +0200

    [uart.c] feat: add messageCapture function

--- TheTicker/Core/Src/uart.c
+++ TheTicker/Core/Src/uart.c
@@ -27,6 +27,7 @@ static osSemaphoreId idleIRQHandle;
 // Static function prototypes
 //---------------------------------------------------------------------------
 static void UART_init(void);
+static uint8_t* messageCapture(USART_TypeDef* usart, uint8_t* sourceBuffer, uint16_t sizeSourceBuffer);
 
 //---------------------------------------------------------------------------
 // Description of peripheral structures
@@ -78,6 +79,87 @@ void idleIRQTask(void const *argument)
 
 		osMessagePut(fromUartToMatrixHandle, (uint32_t)message, osWaitForever);
 	}
+}
+
+//---------------------------------------------------------------------------
+// Static functions
+//---------------------------------------------------------------------------
+
+/**
+ * @brief 	This function parses the RX buffer and grabs messages from it.
+ * @note	Each message has system characters at the end \r and \n. They are not needed for output to the matrix.
+ * 			In the code, these two characters are replaced by one space (' ') character.
+ * @param 	usart - A pointer to U(S)ART peripheral to be used where x is between 1 to 8.
+ * @param 	sourceBuffer - A pointer to RX buffer.
+ * @param 	sizeSourceBuffer - Size of RX buffer.
+ * @retval	A pointer to the created string.
+ */
+static uint8_t* messageCapture(USART_TypeDef* usart, uint8_t* sourceBuffer, uint16_t sizeSourceBuffer)
+{
+	static uint16_t oldPosition	= 0;	// necessary variables to calculate the length of the received message
+	uint16_t position = 0;				// and the index in the RX buffer
+	uint8_t sizeString = 0;
+	uint8_t* string;
+
+	// Get DMA stream
+	DMA_Stream_TypeDef* DMA_Stream = USART_getDmaStream(usart, USART_MODE_RX);
+
+	position = sizeSourceBuffer - DMA_getNumberOfData(DMA_Stream);
+
+	if(position > oldPosition)
+	{
+		sizeString = position - oldPosition;
+
+		/* There are cases when only a service character can be received. And if it is processed as a normal message,
+		   then the program does not work correctly. Therefore, this code is designed to handle such a situation.
+		   When we receive a 1-byte message, we simply return a pointer to a default string. */
+		if(sizeString <= 1)
+		{
+			string = (uint8_t*)defaultString;
+
+		} else if((sizeString > 1) && (sizeString <= OUTPUT_BUFFER_MIN_ROW)) // When only 3 characters need to be transmitted, 4 (3 + '\n') are actually transmitted.
+			   {
+					// Allocate memory for line 4 + 1. 1 for '\0' symbol
+					string = (uint8_t*)pvPortMalloc(((OUTPUT_BUFFER_MIN_ROW + 1) * (sizeof(uint8_t))));
+
+					// Fill the entire string with character ' ' and explicitly specify character '\0' at the last index of the string.
+					memset(string, ' ', OUTPUT_BUFFER_MIN_ROW);
+					string[OUTPUT_BUFFER_MIN_ROW] = '\0';
+
+					// Сopy payload from rx buffer to the string.
+					memcpy(string, &rxBuffer[oldPosition], sizeString - 1); // - '\n'
+
+			    } else
+			    {
+			    	// Allocate memory for line sizeString + 1. 1 for '\0' symbol
+			    	string = (uint8_t*)pvPortMalloc(((sizeString + 1) * sizeof(uint8_t)));
+
+			    	// Fill the entire string with character ' ' and explicitly specify character '\0' at the last index of the string.
+			    	memset(string, ' ', sizeString);
+			    	string[sizeString] = '\0';
+
+			    	// Сopy payload from rx buffer to the string.
+			    	memcpy(string, &sourceBuffer[oldPosition], sizeString - 1); // - '\n'
+			    }
+	} else
+	{
+		if(position > 2)
+		{
+			sizeString = (sizeSourceBuffer - oldPosition) + (position);
+		} else
+		{
+			sizeString = (sizeSourceBuffer - oldPosition);
+		}
+		string = (uint8_t*)pvPortMalloc(((sizeString + 1) * sizeof(uint8_t)));
+		memcpy(string, &sourceBuffer[oldPosition], sizeSourceBuffer - oldPosition);
+		memcpy(&string[sizeSourceBuffer - oldPosition], sourceBuffer, sizeString - (sizeSourceBuffer - oldPosition) - 1);
+	}
+
+	oldPosition = position;
+
+	if(oldPosition == (RX_BUFFER_SIZE - 1)) oldPosition = 0;
+
+	return string;
 }
 
 /**
