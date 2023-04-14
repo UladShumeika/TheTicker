5652d1e13cea88897818773bfe8615f5811c8186
Ulad Shumeika <wladshumeiko228@gmail.com>
2023-04-14 11:50:22 +0200

    [_dma.h/_dma.c] feat: add DMA driver

---
+++ TheTicker/Drivers/Custom/Src/ush_stm32f4xx_dma.c
@@ -0,0 +1,17 @@
+/**
+  ******************************************************************************
+  * @file    ush_stm32f4xx_dma.c
+  * @author  Ulad Shumeika
+  * @version v1.0
+  * @date    04-February-2023
+  * @brief	 This file contains the implementation of functions for working with DMA.
+  *
+  *	NOTE: This file is not a full-fledged DMA driver, but contains only some of
+  * 	  the functions that are needed for the current project.
+  ******************************************************************************
+  */
+
+//---------------------------------------------------------------------------
+// Includes
+//---------------------------------------------------------------------------
+#include "ush_stm32f4xx_dma.h"
