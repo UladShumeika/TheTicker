518bbbee2057b16a913e580b542798cab32e9c4d
Ulad Shumeika <wladshumeiko228@gmail.com>
2023-04-14 16:42:12 +0200

    [_uart.c] feat: add GPIO configuration in USART_init function

--- TheTicker/Drivers/Custom/Src/ush_stm32f4xx_uart.c
+++ TheTicker/Drivers/Custom/Src/ush_stm32f4xx_uart.c
@@ -37,5 +37,83 @@
  */
 void USART_init(USH_USART_initTypeDef *initStructure)
 {
+	USH_GPIO_initTypeDef initGpioStructure = {0,};
+
+	uint16_t tmpReg = 0;
+	uint32_t pclk = 0;
+
+	// Check parameters
+	assert_param(IS_USART_ALL_INSTANCE(initStructure->USARTx));
+	assert_param(IS_USART_PINSPACK(initStructure->PinsPack));
+	assert_param(IS_USART_BAUDRATE(initStructure->BaudRate));
+	assert_param(IS_USART_MODE(initStructure->Mode));
+
+	USH_USART_DISABLE(initStructure->USARTx);
+
+	/* ----------------------- GPIO configuration -------------------------- */
+
+	// Fill in the initGpioStructure to initialize the GPIO pins, these parameters are used for all pinsPacks
+	initGpioStructure.Mode			= GPIO_MODE_ALTERNATE_PP;
+	initGpioStructure.Pull			= GPIO_PULLUP;
+	initGpioStructure.Speed			= GPIO_SPEED_VERY_HIGH;
+
+	if(initStructure->USARTx == USART1)
+	{
+		// USART1 clock enable
+		RCC->APB2ENR |= RCC_APB2ENR_USART1EN;
+
+		if(initStructure->PinsPack == USART_PINSPACK_1) 	// Pins configuration according to pinsPack_1
+		{
+			// GPIOA clock enable
+			RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+
+			// USART1 GPIO pins pack 1 configuration
+			// PA9 	   ------> USART1_TX
+			// PA10    ------> USART1_RX
+			initGpioStructure.GPIOx 		= GPIOA;
+			initGpioStructure.Alternate		= GPIO_AF7_USART1;
+
+			if(initStructure->Mode == USART_MODE_RX_TX)
+			{
+				initGpioStructure.Pin		= (GPIO_PIN_9 | GPIO_PIN_10);
+
+			} else if(initStructure->Mode == USART_MODE_RX)
+			{
+				initGpioStructure.Pin		= GPIO_PIN_10;
+
+			} else					// TX mode selected
+			{
+				initGpioStructure.Pin		= GPIO_PIN_9;
+			}
+
+		} else												// Pins configuration according to pinsPack_2
+		{
+			// GPIOB clock enable
+			RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
+
+			// USART1 GPIO pins pack 1 configuration
+			// PB6 	   ------> USART1_TX
+			// PB7     ------> USART1_RX
+			initGpioStructure.GPIOx 		= GPIOB;
+			initGpioStructure.Alternate		= GPIO_AF7_USART1;
+
+			if(initStructure->Mode == USART_MODE_RX_TX)
+			{
+				initGpioStructure.Pin		= (GPIO_PIN_6 | GPIO_PIN_7);
+
+			} else if(initStructure->Mode == USART_MODE_RX)
+			{
+				initGpioStructure.Pin		= GPIO_PIN_7;
+
+			} else					// TX mode selected
+			{
+				initGpioStructure.Pin		= GPIO_PIN_6;
+			}
+		}
+
+		GPIO_init(&initGpioStructure);
+	}
+
+	//TODO "There are only GPIO settings for USART1 pinsPack1 and pinsPack2"
 
 }
