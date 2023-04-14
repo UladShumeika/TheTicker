5d6c209ff9960dd12a9d30743c94215e91607eec
Ulad Shumeika <wladshumeiko228@gmail.com>
2023-04-14 14:59:30 +0200

    [_conf.h] feat: add USE_FULL_ASSERT section

--- TheTicker/Drivers/Custom/ush_stm32f4xx_conf.h
+++ TheTicker/Drivers/Custom/ush_stm32f4xx_conf.h
@@ -24,4 +24,27 @@
 #include "ush_stm32f4xx_spi.h"
 #include "ush_stm32f4xx_uart.h"
 
+//---------------------------------------------------------------------------
+// Function's parameters check.
+//---------------------------------------------------------------------------
+#ifdef USE_FULL_ASSERT
+
+/**
+  * @brief  The assert_param macro is used for function's parameters check.
+  * @param  expr - If expr is false, it calls assert_failed function
+  *   			   which reports the name of the source file and the source
+  *   			   line number of the call that failed.
+  *   			   If expr is true, it returns no value.
+  * @retval None.
+  */
+  	#define assert_param(expr) ((expr) ? (void)0 : assert_failed((uint8_t *)__FILE__, __LINE__))
+
+//---------------------------------------------------------------------------
+// Function's parameters check.
+//---------------------------------------------------------------------------
+  	void assert_failed(uint8_t* file, uint32_t line);
+#else
+	#define assert_param(expr) ((void)0)
+#endif /* USE_FULL_ASSERT */
+
 #endif	/* __USH_STM32F4xx_CONF_H */
