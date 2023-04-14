db0a121ac9dd1e8ca0da2c7baa7d8a4751ecedf4
Ulad Shumeika <wladshumeiko228@gmail.com>
2023-04-14 17:22:43 +0200

    [uart.c] feat: add UART_init function

--- TheTicker/Core/Src/uart.c
+++ TheTicker/Core/Src/uart.c
@@ -5,12 +5,31 @@
 #include "string.h"
 
 //---------------------------------------------------------------------------
+// Defines
+//---------------------------------------------------------------------------
+
+// Configuration UART
+#define USED_UART		(USART1)
+#define USED_PINSPACK	(USART_PINSPACK_1)
+#define USED_BAUDRATE	((uint32_t)115200)
+
+//---------------------------------------------------------------------------
 // Descriptions of FreeRTOS elements
 //---------------------------------------------------------------------------
 static osThreadId idleIRQTaskHandle;
 static osPoolId	messageStructHandle;
 osMessageQId fromUartToMatrixHandle;
 static osSemaphoreId idleIRQHandle;
+
+//---------------------------------------------------------------------------
+// Static function prototypes
+//---------------------------------------------------------------------------
+static void UART_init(void);
+
+//---------------------------------------------------------------------------
+// Description of peripheral structures
+//--------------------------------------------------------------------------
+USH_USART_initTypeDef uart_structure = {0};
 
 static const uint8_t defaultString[] = "Hi, please enter your message. ";
 
@@ -27,6 +46,8 @@ void idleIRQTask(void const *argument)
 {
 	UART_messageTypeDef *message = (UART_messageTypeDef*)osPoolCAlloc(messageStructHandle);
 	uint8_t flagFirstStart = 1;
+
+	UART_init();
 
 	/* Infinite loop */
 	for(;;)
@@ -45,6 +66,19 @@ void idleIRQTask(void const *argument)
 
 		osMessagePut(fromUartToMatrixHandle, (uint32_t)message, osWaitForever);
 	}
+}
+
+/**
+ * @brief 	This function initializes U(S)ART module.
+ * @retval	None.
+ */
+static void UART_init(void)
+{
+	uart_structure.USARTx 		= USED_UART;
+	uart_structure.PinsPack 	= USED_PINSPACK;
+	uart_structure.BaudRate 	= USED_BAUDRATE;
+	uart_structure.Mode 		= USART_MODE_RX_TX;
+	USART_init(&uart_structure);
 }
 
 //---------------------------------------------------------------------------
