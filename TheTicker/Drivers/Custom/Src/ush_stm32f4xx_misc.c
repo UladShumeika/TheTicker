338e103d5c76f4e52507c5c2a33bae769058a14b
Ulad Shumeika <wladshumeiko228@gmail.com>
2023-04-14 13:38:50 +0200

    [_misc.h/misc.c] feat: add MISC driver
    12

---
+++ TheTicker/Drivers/Custom/Src/ush_stm32f4xx_misc.c
@@ -0,0 +1,17 @@
+/**
+  ******************************************************************************
+  * @file    ush_stm32f4xx_misc.c
+  * @author  Ulad Shumeika
+  * @version v1.0
+  * @date    27-February-2023
+  * @brief	 This file contains the implementation of functions for the miscellaneous
+  * 		 firmware library functions.
+  *
+  ******************************************************************************
+  */
+
+//---------------------------------------------------------------------------
+// Includes
+//---------------------------------------------------------------------------
+#include "ush_stm32f4xx_misc.h"
+#include <stdio.h>
