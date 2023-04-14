105ce9348a5d645bb063ac7b4eb9e49b743cba37
Ulad Shumeika <wladshumeiko228@gmail.com>
2023-04-14 16:02:53 +0200

    [_uart.h] feat: add USH_USART_pinsPack and USH_USART_mode enums

--- TheTicker/Drivers/Custom/Inc/ush_stm32f4xx_uart.h
+++ TheTicker/Drivers/Custom/Inc/ush_stm32f4xx_uart.h
@@ -21,4 +21,27 @@
 //---------------------------------------------------------------------------
 #include "stm32f4xx.h"
 
+//---------------------------------------------------------------------------
+// Structures and enumerations
+//---------------------------------------------------------------------------
+
+/**
+ * @brief USART pinsPack enumeration to select pins combination for U(S)ART
+ */
+typedef enum
+{
+	USART_PINSPACK_1	= 0x00U,	/* pinsPack1 selected */
+	USART_PINSPACK_2				/* pinsPack2 selected */
+} USH_USART_pinsPack;
+
+/**
+ * @brief USART pinsPack enumeration to select a mode for U(S)ART
+ */
+typedef enum
+{
+	USART_MODE_RX		= 0x04U,	/* only RX selected */
+	USART_MODE_TX		= 0x08U,	/* only TX selected */
+	USART_MODE_RX_TX 	= 0x0CU		/* RX and TX selected */
+} USH_USART_mode;
+
 #endif /* __USH_STM32F4XX_USART_H */
